a37944c1d0e5cf1d5bac082ecc06bec5a782ad8a
Zellka <59067641+IlonaZellka@users.noreply.github.com>
2021-01-27 23:50:33 +0300

    Add files via upload

---
+++ course_2/computer_graphics/Laba_graphic_6/Laba_graphic_6/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Laba_graphic_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush roadColor = new SolidBrush(Color.Gray); 
+            SolidBrush lineColor = new SolidBrush(Color.White); 
+            SolidBrush wheelColor = new SolidBrush(Color.Black); 
+            SolidBrush carColor = new SolidBrush(Color.Red);
+            SolidBrush windshieldColor = new SolidBrush(Color.Silver); 
+            SolidBrush truckColor = new SolidBrush(Color.DarkGreen); 
+            SolidBrush cabinColor = new SolidBrush(Color.Orange); 
+            SolidBrush sunColor = new SolidBrush(Color.Yellow); 
+            
+            /*рисуем солнце*/
+            g.FillEllipse(sunColor, 10, 10, 50, 50);
+
+            /*рисуем дорогу*/
+            g.FillRectangle(roadColor, 0, 220, 900, 230);
+            g.FillRectangle(lineColor, 300, 300, 350, 15);
+             
+            DrawTruck(g, truckColor, cabinColor, windshieldColor, wheelColor);
+            DrawCar(g, carColor, windshieldColor, wheelColor);
+
+        }
+        private void DrawTruck(Graphics g, SolidBrush truckColor, SolidBrush cabinColor, SolidBrush windshieldColor, SolidBrush wheelColor)
+        {
+            g.FillEllipse(wheelColor, 310, 215, 70, 70);
+            g.DrawEllipse(Pens.Silver, 310, 215, 70, 70);
+            g.FillEllipse(wheelColor, 360, 215, 70, 70);
+            g.DrawEllipse(Pens.Silver, 360, 215, 70, 70);
+            g.FillEllipse(wheelColor, 470, 215, 70, 70);
+            g.DrawEllipse(Pens.Silver, 470, 215, 70, 70);
+            g.FillEllipse(wheelColor, 520, 215, 70, 70);
+            g.DrawEllipse(Pens.Silver, 520, 215, 70, 70);
+            g.FillRectangle(truckColor, 300, 150, 300, 100);
+            g.FillRectangle(cabinColor, 600, 175, 65, 75);
+            g.FillRectangle(windshieldColor, 630, 175, 35, 45);
+        }
+        private void DrawCar(Graphics g, SolidBrush carColor, SolidBrush windshieldColor, SolidBrush wheelColor)
+        {
+            g.FillRectangle(carColor, 100, 270, 170, 120);
+            g.FillRectangle(carColor, 50, 340, 270, 70);
+            g.FillRectangle(windshieldColor, 170, 270, 100, 45);
+            g.FillEllipse(wheelColor, 60, 370, 70, 70);
+            g.DrawEllipse(Pens.Silver, 60, 370, 70, 70);
+            g.FillEllipse(wheelColor, 240, 370, 70, 70);
+            g.DrawEllipse(Pens.Silver, 240, 370, 70, 70);
+        }
+    }
+}
---
+++ course_2/computer_graphics/Laba_graphic_7/Laba_graphic_7/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Laba_graphic_7
+{
+    public partial class Form1 : Form
+    {
+        Bitmap pictureBoxBitMap;
+        Bitmap spriteBitMap;
+        Bitmap cloneBitMap;
+        Graphics g_pictureBox;
+        Graphics g_sprite;
+        int diametr = 60; 
+        int x, y; 
+        public Form1() { InitializeComponent(); }
+        void DrawSprite()
+        {
+            g_sprite.FillRectangle(Brushes.Black, 0, 0, 10, 10);
+            g_sprite.FillEllipse(Brushes.Black, 0, 0, diametr-1, 10);
+        }
+        void SavePart(int xt, int yt)
+        {
+            Rectangle cloneRect = new Rectangle(xt, yt, diametr, diametr);
+            System.Drawing.Imaging.PixelFormat format =
+            pictureBoxBitMap.PixelFormat;
+            cloneBitMap = pictureBoxBitMap.Clone(cloneRect, format);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"sea1.jpg");
+            pictureBoxBitMap = new Bitmap(pictureBox1.Image);
+            g_pictureBox = Graphics.FromImage(pictureBox1.Image);
+            spriteBitMap = new Bitmap(diametr, 15);
+            g_sprite = Graphics.FromImage(spriteBitMap);
+            DrawSprite();
+            cloneBitMap = new Bitmap(diametr, 15);
+            x = 185; y = pictureBox1.Height / 2;
+            SavePart(x, y);
+            g_pictureBox.DrawImage(spriteBitMap, x, y);
+            pictureBox1.Invalidate();
+            
+            timer1 = new Timer();
+            timer1.Interval = 100;
+            timer1.Tick += new EventHandler(timer1_Tick);
+        }
+        private void button1_Click(object sender, EventArgs e) { timer1.Enabled = true; }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g_pictureBox.DrawImage(cloneBitMap, x, y);
+            x = x + 25;
+            if (x > pictureBox1.Width - diametr)  x = pictureBox1.Location.X; 
+            SavePart(x, y);
+            g_pictureBox.DrawImage(spriteBitMap, x, y);
+            pictureBox1.Invalidate();
+        }
+    }
+}
