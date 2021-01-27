4982072a3fc35eeffc0ab4d44b763b4ece0d6a8a
Zellka <59067641+IlonaZellka@users.noreply.github.com>
2021-01-27 23:51:34 +0300

    Add files via upload

---
+++ course_2/computer_graphics/Laba_graphic_8/Laba_graphic_8/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Laba_graphic_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_money_count = new System.Windows.Forms.Label();
+            this.button_start_game = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button_left = new System.Windows.Forms.Button();
+            this.button_up = new System.Windows.Forms.Button();
+            this.button_right = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1918, 677);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Historic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(612, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ВСЕГО: ";
+            // 
+            // label_money_count
+            // 
+            this.label_money_count.AutoSize = true;
+            this.label_money_count.Font = new System.Drawing.Font("Segoe UI Historic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_money_count.Location = new System.Drawing.Point(750, 9);
+            this.label_money_count.Name = "label_money_count";
+            this.label_money_count.Size = new System.Drawing.Size(0, 38);
+            this.label_money_count.TabIndex = 2;
+            // 
+            // button_start_game
+            // 
+            this.button_start_game.BackColor = System.Drawing.Color.LightGreen;
+            this.button_start_game.Font = new System.Drawing.Font("Segoe UI Historic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_start_game.Location = new System.Drawing.Point(1066, 11);
+            this.button_start_game.Name = "button_start_game";
+            this.button_start_game.Size = new System.Drawing.Size(158, 35);
+            this.button_start_game.TabIndex = 3;
+            this.button_start_game.Text = "Начать игру";
+            this.button_start_game.UseVisualStyleBackColor = false;
+            this.button_start_game.Click += new System.EventHandler(this.button_start_game_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button_left
+            // 
+            this.button_left.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button_left.Font = new System.Drawing.Font("Segoe UI Historic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_left.Location = new System.Drawing.Point(426, 740);
+            this.button_left.Name = "button_left";
+            this.button_left.Size = new System.Drawing.Size(167, 38);
+            this.button_left.TabIndex = 4;
+            this.button_left.Text = "Влево";
+            this.button_left.UseVisualStyleBackColor = false;
+            this.button_left.Click += new System.EventHandler(this.button_left_Click);
+            // 
+            // button_up
+            // 
+            this.button_up.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button_up.Font = new System.Drawing.Font("Segoe UI Historic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_up.Location = new System.Drawing.Point(637, 740);
+            this.button_up.Name = "button_up";
+            this.button_up.Size = new System.Drawing.Size(167, 38);
+            this.button_up.TabIndex = 5;
+            this.button_up.Text = "Вверх";
+            this.button_up.UseVisualStyleBackColor = false;
+            this.button_up.Click += new System.EventHandler(this.button_up_Click);
+            // 
+            // button_right
+            // 
+            this.button_right.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button_right.Font = new System.Drawing.Font("Segoe UI Historic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_right.Location = new System.Drawing.Point(855, 740);
+            this.button_right.Name = "button_right";
+            this.button_right.Size = new System.Drawing.Size(167, 38);
+            this.button_right.TabIndex = 6;
+            this.button_right.Text = "Вправо";
+            this.button_right.UseVisualStyleBackColor = false;
+            this.button_right.Click += new System.EventHandler(this.button_right_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1918, 790);
+            this.Controls.Add(this.button_right);
+            this.Controls.Add(this.button_up);
+            this.Controls.Add(this.button_left);
+            this.Controls.Add(this.button_start_game);
+            this.Controls.Add(this.label_money_count);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Змейка";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_money_count;
+        private System.Windows.Forms.Button button_start_game;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button_left;
+        private System.Windows.Forms.Button button_up;
+        private System.Windows.Forms.Button button_right;
+    }
+}
+
---
+++ course_2/computer_graphics/Laba_graphic_8/Laba_graphic_8/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_graphic_8
+{
+    public partial class Form1 : Form
+    {
+        int moneyCount = 0;
+        PictureBox cobra = new PictureBox();
+        PictureBox man = new PictureBox();
+        PictureBox shrub = new PictureBox();
+        PictureBox fiveCoin = new PictureBox();
+        PictureBox oneCoin = new PictureBox();
+        int manSpeed = 30;
+        Timer timer;
+
+        public Form1()  {  InitializeComponent();  }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"house.jpg");
+            label_money_count.Text = moneyCount.ToString();
+        }
+        private void button_start_game_Click(object sender, EventArgs e)
+        {
+            button_start_game.Visible = false;
+            DrawGamePlatform();
+            timer = new Timer();
+            timer.Interval = 30;
+            timer.Tick += timer1_Tick;
+            timer.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cobra.Left += 2;
+            Rectangle rMan = man.DisplayRectangle;
+            rMan.Location = man.Location;
+            Rectangle rCobra = cobra.DisplayRectangle;
+            rCobra.Location = cobra.Location;
+            if (rCobra.IntersectsWith(rMan))
+            {
+                timer.Stop();
+                LosingMessage();
+            }
+        }
+
+        private void button_right_Click(object sender, EventArgs e)
+        {
+            if (man.Left + man.Width + manSpeed < pictureBox1.Image.Width) 
+            {
+                man.Left += manSpeed;
+                Collision();
+            }  
+            else VictoryMessage();
+        }
+        private void button_up_Click(object sender, EventArgs e)
+        {
+            if (man.Left + man.Width + manSpeed < pictureBox1.Image.Width-50)
+            {
+                man.Top -= 60;
+                man.Left += 280;
+                System.Threading.Thread.Sleep(100);
+                man.Top += 60;
+                Collision();
+            }
+            else VictoryMessage();
+        }
+        private void button_left_Click(object sender, EventArgs e)
+        {
+            if (man.Left >= manSpeed)
+            {
+                man.Left -= 20;
+                Collision();
+            }
+        }
+        private void VictoryMessage()
+        {
+            man.Dispose();
+            DialogResult result = MessageBox.Show("ПОБЕДА!");
+            if (result == DialogResult.OK) this.Close();
+        }
+        private void LosingMessage()
+        {
+            man.Dispose();
+            DialogResult result = MessageBox.Show("ПРОИГРЫШ");
+            if (result == DialogResult.OK) this.Close();
+        }
+        private void Collision()
+        {
+            Rectangle rMan = man.DisplayRectangle;
+            rMan.Location = man.Location;
+            Rectangle rFiveCoin = fiveCoin.DisplayRectangle;
+            rFiveCoin.Location = fiveCoin.Location;
+            Rectangle rOneCoin = oneCoin.DisplayRectangle;
+            rOneCoin.Location = oneCoin.Location;
+            Rectangle rShrub = shrub.DisplayRectangle;
+            rShrub.Location = shrub.Location;
+            if (rMan.IntersectsWith(rFiveCoin) && moneyCount < 60)
+            {
+                fiveCoin.Dispose();
+                moneyCount += 50;
+            }
+            if (rMan.IntersectsWith(rOneCoin) && moneyCount < 10)
+            {
+                oneCoin.Dispose();
+                moneyCount += 10;
+            }
+            if (rMan.IntersectsWith(rShrub))
+            {
+                LosingMessage();
+            }
+            label_money_count.Text = moneyCount.ToString();
+        }
+
+        private void DrawGamePlatform()
+        {
+            man.Image = Image.FromFile(@"man.png");
+            man.BackColor = Color.Transparent;
+            man.Location = new Point(0, 370);
+            man.Size = new Size(man.Image.Width, man.Image.Height);
+            pictureBox1.Controls.Add(man);
+            man.BringToFront();
+
+            cobra.Image = Image.FromFile(@"cobra.png");
+            cobra.BackColor = Color.Transparent;
+            cobra.Location = new Point(-250, 400);
+            cobra.Size = new Size(cobra.Image.Width, cobra.Image.Height);
+            pictureBox1.Controls.Add(cobra);
+            cobra.BringToFront();
+
+            shrub.Image = Image.FromFile(@"shrub.png");
+            shrub.BackColor = Color.Transparent;
+            shrub.Location = new Point(350, 450);
+            shrub.Size = new Size(shrub.Image.Width, shrub.Image.Height);
+            pictureBox1.Controls.Add(shrub);
+            shrub.BringToFront();
+
+            oneCoin.Image = Image.FromFile(@"one.png");
+            oneCoin.BackColor = Color.Transparent;
+            oneCoin.Location = new Point(550, 450);
+            oneCoin.Size = new Size(oneCoin.Image.Width, oneCoin.Image.Height);
+            pictureBox1.Controls.Add(oneCoin);
+            oneCoin.BringToFront();
+
+            fiveCoin.Image = Image.FromFile(@"five.png");
+            fiveCoin.BackColor = Color.Transparent;
+            fiveCoin.Location = new Point(820, 450);
+            fiveCoin.Size = new Size(fiveCoin.Image.Width, fiveCoin.Image.Height);
+            pictureBox1.Controls.Add(fiveCoin);
+            fiveCoin.BringToFront();
+        }
+    }
+}
