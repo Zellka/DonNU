be61c2ffa3571a52aa152c463d36001a72649711
Ilona Zelinskaya <59067641+Zellka@users.noreply.github.com>
2021-02-14 14:00:22 +0300

    Update Lab1.py

--- course_2/python/Lab1.py
+++ course_2/python/Lab1.py
@@ -165,3 +165,23 @@ def extra_enumerate(list):
         yield i, cum, frac
 
 [print((elem, cum, frac), end="  ") for elem, cum, frac in extra_enumerate([1, 3, 4, 2])]
+
+#Напишите декоратор non_empty, который дополнительно проверяет
+#списковый результат любой функции: если в нем содержатся пустые
+#строки или значение None, то они удаляются. Пример кода:
+#@non_empty
+#def get_pages():
+ #return ['chapter1', '', 'contents', '', 'line1']. --14
+
+def non_empty(fn):
+    def wrapped():
+        return list(filter(None, fn()))
+    return wrapped
+
+
+@non_empty
+def get_pages():
+    return ['chapter1', '', 'contents', '', 'line1']
+
+
+print(get_pages())
