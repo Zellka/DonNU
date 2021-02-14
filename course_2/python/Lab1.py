8bc29eb9159ca74b08a30f5fd8b44590ea0ed579
Ilona Zelinskaya <59067641+Zellka@users.noreply.github.com>
2021-02-14 13:16:32 +0300

    Update Lab1.py

--- course_2/python/Lab1.py
+++ course_2/python/Lab1.py
@@ -133,3 +133,16 @@ def frange(start, stop, step):
         start += step
 
 [print(i, end = "\n") for i in frange(1, 5, 0.1)]
+
+#Напишите генератор get_frames(), который производит «оконную
+#декомпозицию» сигнала: на основе входного списка генерирует набор
+#списков – перекрывающихся отдельных фрагментов сигнала размера
+#size со степенью перекрытия overlap. --12 
+
+def get_frames(signal, size, overlap):
+    step = int(size * overlap)
+    stop = len(signal) - 1
+    for start in range(0, stop, step):
+        yield [i for i in signal[start : start + size] ]
+
+[print(frame) for frame in get_frames(range(10),2,0.5)]
