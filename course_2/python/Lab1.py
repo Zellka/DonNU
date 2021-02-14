9af65846a970dc941aefa9fb27f1ad190521a2ea
Ilona Zelinskaya <59067641+Zellka@users.noreply.github.com>
2021-02-14 15:35:53 +0300

    Update Lab1.py

--- course_2/python/Lab1.py
+++ course_2/python/Lab1.py
@@ -181,3 +181,16 @@ print(get_pages())
 #Напишите параметризированный декоратор pre_process, который осуществляет предварительную обработку (цифровую фильтрацию)
 #списка по алгоритму: s[i] = s[i]–a∙s[i–1]. Параметр а можно задать в коде (по умолчанию равен 0.97). --15
     
+def pre_process(a):
+    def decorator(fn):
+        def wrapped(list):
+            return  fn([round(i-a*(i-1),2) for i in list])
+        return wrapped
+    return decorator
+
+@pre_process(0.97)
+def plot_signal(list):
+    [print(sample) for sample in list]
+
+plot_signal([i for i in range(15)])
+    
