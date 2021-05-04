a410dc06ecf80c4eae00d11bb05d6798fb4c92ed
Ilona Zelinskaya <59067641+Zellka@users.noreply.github.com>
2021-05-04 18:50:06 +0300

    Update 5lab.asm

--- course_2/assembler/2semester/5lab.asm
+++ course_2/assembler/2semester/5lab.asm
@@ -1,107 +1,133 @@
-yulya segment para 'code'
-assume cs:yulya, ds:yulya,es:yulya,ss:yulya
+;Y = (1+x*x)*arctg(x)/2
+.286
+.model small
+.data
+dnd segment 'code'
+assume cs:dnd, ds:dnd, ss:dnd, es:dnd
 org 100h
 begin: jmp main
-;----------------------------------------------------------
-; выделение и инициализация ячеек оперативной памяти
-s db 'Number of mouse keys = $'
-nr db ?
-s0 db 13,10,'The following mouse type is installed:',13,10,'$'
-s1 db '1 - Bus Mouse',13,10,'$'
-s2 db '2 - Serial Mouse',13,10,'$'
-s3 db '3 - Inport Mouse',13,10,'$'
-s4 db '4 - PS/2 Mouse',13,10,'$'
-s5 db '5 - HP Mouse',13,10,'$'
-X1 dw ?
-Y1 dw ?
-X db ?
-Y db ?
-MN db 8
-s_k db 5 DUP(?)
- 
-; ---------------------------------------------------------
+;-------Данные -------------------
+;mov ax, @data
+;mov ds, ax
+
+X dq 1.3
+Y dq ?
+
+DWA dq 2.0
+TIME dq ?
+TEM dq ?
+
+my_s db '+'
+T_Th db ?
+Th db ?
+Hu db ?
+Tens db ?
+Ones db ?
+
+;---------------------------------
 main proc near
-; получение текущего видеорежима
-mov ah,0fh
-int 10h
-mov nr,al ; nr - номер режима
-; инициализация мыши
-mov ax,0000h
-int 33h
-; вывод количества клавиш мыши
-mov ah,09
-lea dx,s
-int 21h
-mov ah,02
-mov dl,bl
-add dl,48
-int 21h
-; Получение информации о типе мыши ***************
-mov ah,09
-lea dx,s0
-int 21h
-mov ax,0024h
-int 33h
-; вывод информации о типе мыши ***************
-cmp ch,1
+;-------Команды программы --------
+FINIT ; инициализация математического сопроцессора
+FLD X 		; ST(0) = x = 1.3
+FLD X		; ST(0) = x = 1.3; ST(1) = x = 1.3
+FMUL 		; ST(0) = x*x
+FLD1 		; ST(0) =1; ST(1) = x*x = 1.69
+FADD 		; ST(0) = x*x +1 = 2.69
+FSTP TIME 	; TIME = x*x +1 = 2.69
+FLD X 		; ST(0) = x = 1.3
+FPATAN		; ST(0) = 1; ST(1) = 0.915
+FSTP X 		; X = 1
+FSTP TEM 	; TEM= 0.915
+FLD TEM		; ST(0) = 0.915
+FLD DWA	; ST(0) = 2; ST(1) = 0.915
+FDIV		; ST(0) = 0.915/2 = 0.4725
+FLD TIME		; ST(0) = x*x +1  =2.69; ST(1) = 0.915/2 = 0.4725
+FMUL		; ST(0) = 2.69 * 0.4725 = 1.27
+FSTP Y 		; Y= 1.27
 
-jne @z2
-; 1 тип
-mov ah,09
-lea dx,s1
-int 21h
-jmp @z100
-@z2: cmp ch,2
-jne @z3
-; 2 тип
-mov ah,09
-lea dx,s2
-int 21h
-jmp @z100
-@z3: cmp ch,3
-jne @z4
-; 3 тип
-mov ah,09
-lea dx,s3
-int 21h
-jmp @z100
-@z4: cmp ch,4
-jne @z5
-; 4 тип
-mov ah,09
-lea dx,s4
-int 21h
-jmp @z100
-@z5: cmp ch,5
-jne @z100
-; 5 тип
-mov ah,09
-lea dx,s5
-int 21h
-@z100:
-; ожидание нажатия любой клавиши
-mov ah,0 ; получаем номер клавиши
-int 16h
-; выключить курсор
-mov ah,01
-mov ch,20h
-int 10h
-; включить курсор мыши
-mov ax,0001
-int 33h
+FLD Y
+push 10
+call outfloat
 
-;изменяем форму мыши
-mov ax, 000Ah
-int 33h
-mov bx, 1110
-mov cx, 0
-mov dx, 01418h
+; аргумент — количество цифр дробной части
+length_frac equ [bp+4]
+; локальные переменные
+ten equ word ptr [bp-2]
+temp equ word ptr [bp-4]
 
+ret
+main endp
+
+OutFloat proc near 
+enter 4, 0 
+mov ten, 10 
+ftst 
+fstsw ax 
+sahf 
+jnc @positiv 
+mov al, '-'
+int 29h 
+fchs
+@positiv:
+fld1 
+fld st(1)
+fprem
+fsub st(2), st 
+fxch st(2)
+xor cx, cx 
+@1: 
+fidiv ten 
+fxch st(1) 
+fld st(1)
+fprem 
+fsub st(2), st 
+fimul ten
+fistp temp
+push temp
+inc cx 
+fxch st(1)
+ftst
+fstsw ax 
+sahf 
+jnz @1
+@2:
+pop ax 
+add al, '0' 
+int 29h
+loop @2 
+
+fstp st 
+fxch st(1) 
+ftst 
+fstsw ax 
+sahf 
+jz @quit 
+mov al, '.' 
+int 29h 
+mov cx, length_frac
+@3: 
+fimul ten
+fxch st(1)
+fld st(1)
+fprem
+fsub st(2), st
+fxch st(2) 
+fistp temp
+mov ax, temp
+int 29h
+fxch st(1)
+ftst 
+fstsw ax 
+sahf
+loopne @3 
+@quit: 
+fstp 
+fstp st 
+leave
 mov ah,08 ;ожидание нажатия клавиши
 int 21h
+ret 2 
+OutFloat endp
 
-; ******************************************************
-ret ; возврат в операционную систему
-main endp
-yulya ends
+dnd ends
 end begin
