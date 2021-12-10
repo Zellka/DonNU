17c640187deb0460d2920b60bf851c38c6af6a4e
Ilona Zelinskaya <ilona.zelka@gmail.com>
2021-12-10 13:53:44 +0300

    Update Ex.cpp

--- course_3/computational_math/Lab5/Lab5/Ex.cpp
+++ course_3/computational_math/Lab5/Lab5/Ex.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
 
     T[0] = a; V[0] = 2;
 
-    //метод Эйлера
+    //РјРµС‚РѕРґ Р­Р№Р»РµСЂР°
     for (int i = 1; i <= n - 1; i++) {
         T[i] = a + i * h;
         V[i] = V[i - 1] + h * F(T[i - 1], V[i - 1]);
@@ -29,12 +29,14 @@ int main(int argc, char** argv)
         output << T[i] << '\t' << V[i] << std::endl;
     }
 
+    // std::unique_ptr<double[]> T(new double[(int)n]);
+    //std::unique_ptr<double[]> V(new double[(int)n]);
     std::unique_ptr<double[]> V1(new double[(int)n]);
     std::unique_ptr<double[]> V2(new double[(int)n]);
     std::unique_ptr<double[]> V3(new double[(int)n]);
     std::unique_ptr<double[]> V4(new double[(int)n]);
 
-    //метод Рунге-Кутта 4-го порядка
+    //РјРµС‚РѕРґ Р СѓРЅРіРµ-РљСѓС‚С‚Р°
     for (int i = 1; i <= n-1; i++) {
         T[i] = a + i * h;
         V1[i] = h * F(T[i - 1], V[i - 1]);
@@ -45,7 +47,7 @@ int main(int argc, char** argv)
     }
 
     std::ofstream output;
-    output.open("Ex1_Runge–Kutta.txt");
+    output.open("Ex1_RungeвЂ“Kutta.txt");
     for (int i = 0; i <= n-1; i++) {
         output << T[i] << '\t' << V[i] << std::endl;
     }
