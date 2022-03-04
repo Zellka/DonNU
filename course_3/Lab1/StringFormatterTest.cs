ad78c1d1c6d444b3fe7f0d9a8778241b65783029
Ilona Zelinskaya <ilona.zelka@gmail.com>
2022-03-04 20:12:32 +0300

    Add files via upload

---
+++ course_3/Lab1/StringFormatterTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Lab1
+{
+    class StringFormatterTest
+    {
+        StringFormatter stringFormatter;
+
+        [SetUp]
+        public void Setup()
+        {
+            stringFormatter = new StringFormatter();
+        }
+
+        [Test]
+        public void SafeStringNotCorrectTest()
+        {
+            Assert.Throws<NullReferenceException>(() => stringFormatter.SafeString(null));
+        }
+
+        [TestCase("'select * from Order'", ExpectedResult = "''SELECT * from Order''")]
+        [TestCase("'insert * from Order'", ExpectedResult = "''INSERT * from Order''")]
+        [TestCase("'update * from Order'", ExpectedResult = "''UPDATE * from Order''")]
+        [TestCase("'delete * from Order'", ExpectedResult = "''DELETE * from Order''")]
+        [TestCase("", ExpectedResult = "")]
+        public string SafeStringCorrectTest(string s)
+        {
+            return stringFormatter.SafeString(s);
+        }
+    }
+}
