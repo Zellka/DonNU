885c9db5334309470807da6006b3a62561d9c1b1
Ilona Zelinskaya <ilona.zelka@gmail.com>
2022-03-04 20:49:40 +0300

    Add files via upload

---
+++ course_3/Testing/Lab1/ArrayProcessor.cs
@@ -0,0 +1,43 @@
+﻿/*Написать тесты с помощью NUnit для решения задачи, в соответствии с вариантом, с
+Assert’ами коллекций*/
+
+/*(2-3) В Production Code написать класс ArrayProcessor с методом int[] SortAndFilter(int[] a),
+который формирует новый массив на основе входного: сортирует массив (не затрагивая
+исходный) и удаляет из него все повторяющиеся отрицательные элементы (оставляя по
+одному). В Test Code написать тесты, которые проверяют соответствие результатов
+метода спецификации требований */
+
+using System;
+using System.Linq;
+
+namespace Lab1
+{
+    class ArrayProcessor
+    {
+        public int[] SortAndFilter(int[] a)
+        {
+            int[] arrayNegative = Array.FindAll(a, (val) => val < 0).Distinct().ToArray();
+            int[] b = Array.FindAll(a, (val) => val >= 0).Concat(arrayNegative).ToArray();
+            Sort(ref b);
+            return b;
+        }
+
+
+        public void Sort(ref int[] a)
+        {
+            int tmp;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] < a[j])
+                    {
+                        tmp = a[i];
+                        a[i] = a[j];
+                        a[j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ course_3/Testing/Lab1/ArrayProcessorTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Lab1
+{
+    [TestFixture]
+    class ArrayProcessorTest
+    {
+        ArrayProcessor array;
+        [SetUp]
+        public void Setup()
+        {
+            array = new ArrayProcessor();
+        }
+
+        [Test]
+        public void SortAndFilterTest()
+        {
+            int[] a = { 4, -7, 1, -7, 2, 2, -3};
+            int[] b = { -7, -3, 1, 2, 2, 4};
+            CollectionAssert.AreEquivalent(array.SortAndFilter(a), b);
+        }
+    }
+}
